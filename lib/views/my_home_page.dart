aa777a0b96737311b05e5dabc9fb21f3857c7886
hayate <hayate.k.0704@gmail.com>
2024-06-30 00:43:27 +0900

    update: 画面タイトルと、テキストフィールドをデザイン通りに実装

--- lib/views/my_home_page.dart
+++ lib/views/my_home_page.dart
@@ -14,34 +14,32 @@ class MyHomePage extends ConsumerWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('ToDo List'),
+        title: Text('タスク一覧'),
+        titleTextStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0),
       ),
       body: Column(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              children: [
-                Expanded(
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
+                child: Expanded(
                   child: TextField(
                     controller: _textController,
                     decoration: InputDecoration(
-                      labelText: 'Add a new task',
+                      prefixIcon: const Icon(Icons.search, size: 24),
+                      prefixIconColor: Color(0xFF45A182),
+                      hintText: 'タスクを検索する',
+                      hintStyle: TextStyle(color: Color(0xFF45A182), fontSize: 16.0),
+                      fillColor: Color(0xFFE5F5F0),
+                      filled: true,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                        borderSide: BorderSide.none
+                      ),
                     ),
+                    keyboardType: TextInputType.text,
                   ),
                 ),
-                IconButton(
-                  icon: Icon(Icons.add),
-                  onPressed: () {
-                    if (_textController.text.isNotEmpty) {
-                      todoListNotifier.addTodoItem(_textController.text);
-                      _textController.clear();
-                    }
-                  },
-                ),
-              ],
-            ),
-          ),
+              ),
           Expanded(
             child: ListView.builder(
               itemCount: todoList.length,
